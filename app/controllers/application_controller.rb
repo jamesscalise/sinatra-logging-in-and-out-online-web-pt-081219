890227bb7bdee727ee1c999b9acafb2268b45b16
James Scalise <jamescscalise@gmail.com>
2019-12-08 04:58:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,8 +20,9 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
+    @user = User.find(session[:user_id])
     if @user
-      @user = User.find(session[:user_id])
+      
       erb :account
     end
     erb :error
